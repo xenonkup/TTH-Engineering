716534dbf9808b00f60335bad262a1c125dc038b
xenonkup <tiger2000@gmail.com>
2024-08-06 13:41:50 +0700

    js 1

--- static/js/about.js
+++ static/js/about.js
@@ -1,6 +1,13 @@
-// static/js/about.js
 document.addEventListener('DOMContentLoaded', (event) => {
     const video = document.getElementById('myVideo');
+
+    // Check if the video element is muted and play the video
+    if (video.muted) {
+        video.muted = true;
+        video.play().catch(error => {
+            console.error('Error attempting to play the video:', error);
+        });
+    }
 
     // Unmute the video after user interaction
     document.body.addEventListener('click', () => {
